32fe19e93a7c1b4691ca13e302838c852a3d8426
john <john@nas2.jcashomelab.com>
2022-11-14 12:58:48 -0500

    adding env vars & config file

---
+++ configurator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import os
+from pathlib import Path
+
+
+def get_env_vars():
+    env_vars = {}
+
+    for k, v in os.environ.items():
+        env_vars[k] = v
+
+    return env_vars
+
+
+def get_config(config_path):
+    config = {}
+
+    with open(config_path) as f:
+        for line in f:
+            line = line.strip()
+            _values = line.split("=")
+
+            config[_values[0].strip()] = _values[1].strip()
+
+    return config
+
+
+# print(get_env_vars())
+# print(get_config(Path("config.ini")))
